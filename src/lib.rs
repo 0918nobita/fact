2d12890a0609d577becb91521264b6d089ab97e4
0918nobita <nobita.0918@gmail.com>
2020-07-16 22:30:21 +0900

    :art: Format code

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,11 @@ fn fact_cps<R: 'static>(n: u32, k: Box<dyn FnOnce(u32) -> R>) -> R {
     if n == 0 {
         k(1)
     } else {
-        fact_cps(n - 1, Box::new(move |x| { k(n * x) }))
+        fact_cps(n - 1, Box::new(move |x| k(n * x)))
     }
 }
 
 #[no_mangle]
-pub extern fn fact(n: u32) -> u32 {
+pub extern "C" fn fact(n: u32) -> u32 {
     fact_cps(n, Box::new(identity))
 }
